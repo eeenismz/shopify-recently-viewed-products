62480f2fcf41c9ca7c7873b230402f7e51707e8a
Ee <eeenismz@gmail.com>
2023-04-01 02:00:30 +0700

    Put this file into your asset folder

---
+++ products.recently-viewed.js
@@ -0,0 +1,51 @@
+Shopify.shop_name = Shopify.shop.split('.')[0]
+Shopify.recentProductHistoryMax = 10;
+Shopify.recentProductsKey = Shopify.shop_name+'.products.recent_products';
+
+Shopify.RecentlyViewed = (function() {
+
+  function storeRecentProducts(recentProducts) {
+    window.localStorage.setItem(Shopify.recentProductsKey, JSON.stringify(recentProducts));
+  };
+  function getRecentProducts() {
+    var existingValue = window.localStorage.getItem(Shopify.recentProductsKey);
+    if (existingValue) {
+      try {
+        return JSON.parse(existingValue);
+      } catch (error) {}
+    }
+    return [];
+  };
+
+  function RecentlyViewed(container) {
+    this.$container = $(container);
+    
+    var productData = {
+      title: this.$container.data('title'),
+      id: this.$container.data('id'),
+      grid_item: this.$container.data('grid-item'),
+    };
+    
+    // make sure that the current product does not show on the recent products list
+    // happens when one goes back
+    var existing = getRecentProducts().filter(e=>e.id !== productData.id);
+    
+    if (existing.length > 1) {
+          // Limit the number of recent products
+          if (existing.length > Shopify.recentProductHistoryMax){
+            existing.length = Shopify.recentProductHistoryMax;
+          }
+          this.$container.removeAttr('hidden');
+        }
+    // add the current product to thelist of recent products
+    existing.unshift(productData);    
+    storeRecentProducts(existing)
+  }
+  return RecentlyViewed;
+})();
+
+var Shopify = Shopify || {};
+
+if ($(".product-recently-viewed").length>0){
+  Shopify.RecentlyViewed(".product-recently-viewed")
+};
